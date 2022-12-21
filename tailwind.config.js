4d4cfe7b7204eeb1bc0b8527bf7fcad2635ad53c
Michal Kupczak <kurczak12333@gmail.com>
2022-12-21 15:23:52 +0100

    fix: CI/CD build v2

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    './docs/**/*.{js,ts,vue,md}',
-    './node_modules/vuepress-theme-bcc-common-components/**/*.{js,ts,vue,md}',
-  ],
-  plugins: [require('flowbite/plugin')],
-  theme: {},
-}
